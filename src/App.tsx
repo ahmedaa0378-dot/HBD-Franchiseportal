ed1468dcdc375b6a81249ee93402f06d49116fb2
ahmedaa0378-dot <ahmed.aa0378@gmail.com>
2026-04-05 17:11:53 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useApp } from './context/AppContext';
 import DeliverySettingsPage from './pages/admin/DeliverySettingsPage';
+import PaymentSettingsPage from './pages/admin/PaymentSettingsPage';
 
 // Franchise Pages
 import LoginPage from './pages/LoginPage';
